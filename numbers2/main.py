a8185687e63b07b806a1a22e30f6a901bd4b1480
AliCroc <artur.ajchsztet@gmail.com>
2022-01-09 10:41:45 +0100

    1/2 number2 completed

---
+++ numbers2/main.py
@@ -0,0 +1,39 @@
+
+# imports
+
+
+# helping variables / functions
+
+hex_list =  ["A", "B", "C", "D", "E", "F"]
+
+# task functions
+
+def as_binary(n):
+    return "{0:b}".format(int(n))
+
+    # previous solution:
+    # result = ""
+    # while n > 0:
+    #     m = n % 2
+    #     n = n // 2
+    #     result = str(m) + result
+    # return result
+
+
+def as_hex(n):
+    result = ""
+    while n > 0:
+        m = n % 16
+        n = n // 16
+        if m >= 10:
+            result = hex_list[m-10] + result
+        else:
+            result = str(m) + result
+    return result
+
+# test zone
+
+print(as_binary(7))
+print(as_hex(15), as_hex(255))
+print("e")
+print("e")
